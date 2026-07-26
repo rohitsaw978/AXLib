017aae53b441c2d383112c5054fe2ded5f8732ee
rohitsaw978 <rohitsaw09876@gmail.com>
2026-07-26 23:58:43 +0530

    ready to build

--- frontend/src/pages/user/register.jsx
+++ frontend/src/pages/user/register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import axios from "axios";
-import "./Register.css";
+import "./register.css";
 import {
   FaUser,
   FaEnvelope,
